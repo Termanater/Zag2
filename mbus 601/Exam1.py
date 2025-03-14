e6b01798ee408143222ea49b522e4d28100883c2
Termanater <Ntyler@zagmail.gonzaga.edu>
2025-03-14 16:44:26 -0700

    exam 1

--- mbus 601/Exam1.py
+++ mbus 601/Exam1.py
@@ -16,17 +16,75 @@
 # Type your code here. 
 
 
-TI = input().split()
-wages = int(TI[0])
-taxable_intrest = int(TI[1])
-unemployment = int(TI[2])
-status = int(TI[3])
-withholding = int(TI[4])
+# Input wages0, taxable interest1, unemployment compensation2, status3 (1=single and 2=married) and taxes withheld4 as integers.
+GI = input().split()
+wages = int(GI[0])
+taxable_interest = int(GI[1])
+unemployment = int(GI[2])
+status = int(GI[3])
+withholding = int(GI[4])
 #Calculate and output AGI (wages + interest + unemployment). 
-AGI = (wages + taxable_intrest +   unemployment)
-
+AGI = (wages + taxable_interest +   unemployment)
+#Output error message if AGI is above $120,000 and the program stops with no additional output
 if AGI > 120000:
-    print(f' AGI: ${AGI:,}\nError: Income too high to use this form')
-    break
+    print(f'AGI: ${AGI:,}\nError: Income too high to use this form')
 else:
-    print(f'AGI: ${AGI}')
+    print(f'AGI: ${AGI:,}')
+#deduction amount based on status: (1) Single=$12,000 or (2) Married=$24,000.
+    if status == 1:
+        deduction = 12000
+    elif status == 2:
+        deduction = 24000
+    else:
+        status = 1 
+        deduction = 12000
+#Set status to 1 if not input as 1 or 2.
+#calc taxable income (AGI - deduction)
+    Taxable_income = AGI - deduction
+    if Taxable_income < 0:
+        Taxable_income = 0
+    print(f'Deduction: ${deduction:,}')
+    print(f'Taxable income: ${Taxable_income:,}')
+    if deduction == 12000:
+        if Taxable_income <= 10000:
+            tax = Taxable_income*.10
+            print(f'Federal tax: ${round(tax):,}')
+        elif Taxable_income <40000:
+            tax = 1000 + .12 * (Taxable_income - 10000)
+            print(f'Federal tax: ${round(tax):,}')
+        elif Taxable_income <= 85000:
+            tax = 4600 + .22 *(Taxable_income - 40000)
+            print(f'Federal tax: ${round(tax):,}')
+        else:
+            tax = 14500 + .24 * (Taxable_income - 85000)
+            print(f'Federal tax: ${round(tax):,}')       
+    elif deduction == 24000: 
+        if Taxable_income <= 20000:
+            tax = Taxable_income*.10
+            print(f'Federal tax: ${round(tax):,}')
+        elif Taxable_income <= 80000:
+            tax = 2000 + .12 * (Taxable_income - 20000)
+            print(f'Federal tax: ${round(tax):,}')
+        else:
+            tax = 9200 + .22 * (Taxable_income - 80000)
+            print(f'Federal tax: ${round(tax):,}')
+    Tax_result = (tax-withholding)
+    if Tax_result <= 0:
+        print(f'Tax refund: ${abs(Tax_result):,}')
+    else:
+        print(f'Tax Due: {abs(Tax_result):,}')        
+#Calculate amount of tax due (tax - withheld). If the amount due is negative refund
+#Output tax due or tax refund as positive values
+            
+
+
+
+    
+         
+        
+        
+    #if deduction == 24000 and Taxable_income < 10001 or Taxable_income >= 40000
+
+#Calculate tax amount based on exemption and taxable income
+#Tax amount should be stored as a double and rounded to the nearest whole number using round()
+# Set taxable income to zero if negative. Output deduction and taxable income.
